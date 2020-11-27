2354217ca8927f36897f82d126f109e7ac818baf
ayub6717 <ayub15-6717@diu.edu.bd>
2020-11-27 10:41:12 +0600

    ss

--- src/components/FooterComponent/FooterComponent.js
+++ src/components/FooterComponent/FooterComponent.js
@@ -8,11 +8,7 @@ function FooterComponent() {
 				<h5>Contacts</h5>
 				<p>MD.AYUB SHAMIM</p>
 				<p>ahmed1.ayub6717@gmail.com</p>
-<<<<<<< HEAD
-				<p>01001010101010</p>
-=======
 				<p>+8801731357071</p>
->>>>>>> 8ca400f0496a325416ae1bd24ba0cafb018ae2cf
 			</div>
 			<div className="col-md-4">
 				<h5>Social Links</h5>
