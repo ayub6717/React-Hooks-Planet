535a89f3afb9f38c0173ed5446b25d01c55516a5
ayub6717 <ayub15-6717@diu.edu.bd>
2020-11-27 10:20:07 +0600

    few changed

--- src/components/FooterComponent/FooterComponent.js
+++ src/components/FooterComponent/FooterComponent.js
@@ -6,15 +6,13 @@ function FooterComponent() {
 		<div className="footer row">
 			<div className="col-md-4">
 				<h5>Contacts</h5>
-				<p>Shakhawat Hossain</p>
-				<p>shshawon39@gmail.com</p>
+				<p>MD.AYUB SHAMIM</p>
+				<p>ahmed1.ayub6717@gmail.com</p>
 				<p>01001010101010</p>
 			</div>
 			<div className="col-md-4">
 				<h5>Social Links</h5>
-				<p>facebook.com/sh.shawon1</p>
-				<p>github.com/shawon39</p>
-				<p>twitter.com/shshawon39</p>
+				<p>github.com/ayub6717</p>
 			</div>
 			<div className="col-md-4">
 				<h5>Company</h5>
